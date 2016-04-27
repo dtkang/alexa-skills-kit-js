1758d17e3ac60921fbe0a02dad76289ac3a14c76
dtkang <dtkang@gmail.com>
2016-04-26 20:33:43 -0500

    Updated index.js for baby trivia

--- samples/reindeerGames/src/index.js
+++ samples/reindeerGames/src/index.js
@@ -21,247 +21,146 @@
  */
 var questions = [
     {
-        "Reindeer have very thick coats, how many hairs per square inch do they have?": [
-            "13,000",
-            "1,200",
-            "5,000",
-            "700",
-            "1,000",
-            "120,000"
-        ]
-    },
-    {
-        "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in:": [
-            "Japan",
-            "United States",
-            "Finland",
-            "Germany"
-        ]
-    },
-    {
-        "Santas reindeer are cared for by one of the Christmas elves, what is his name?": [
-            "Wunorse Openslae",
-            "Alabaster Snowball",
-            "Bushy Evergreen",
-            "Pepper Minstix"
-        ]
-    },
-    {
-        "If all of Santas reindeer had antlers while pulling his Christmas sleigh, they would all be:": [
-            "Girls",
-            "Boys",
-            "Girls and boys",
-            "No way to tell"
-        ]
-    },
-    {
-        "What do Reindeer eat?": [
-            "Lichen",
-            "Grasses",
-            "Leaves",
-            "Berries"
-        ]
-    },
-    {
-        "What of the following is not true?": [
-            "Caribou live on all continents",
-            "Both reindeer and Caribou are the same species",
-            "Caribou are bigger than reindeer",
-            "Reindeer live in Scandinavia and Russia"
-        ]
-    },
-    {
-        "In what year did Rudolph make his television debut?": [
-            "1964",
-            "1979",
-            "2000",
-            "1956"
-        ]
-    },
-    {
-        "Who was the voice of Rudolph in the 1964 classic?": [
-            "Billie Mae Richards",
-            "Burl Ives",
-            "Paul Soles",
-            "Lady Gaga"
-        ]
-    },
-    {
-        "In 1939 what retailer used the story of Rudolph the Red Nose Reindeer?": [
-            "Montgomery Ward",
-            "Sears",
-            "Macys",
-            "Kmart"
-        ]
-    },
-    {
-        "Santa's reindeer named Donner was originally named what?": [
-            "Dunder",
-            "Donny",
-            "Dweedle",
-            "Dreamy"
-        ]
-    },
-    {
-        "Who invented the story of Rudolph?": [
-            "Robert May",
-            "Johnny Marks",
-            "Santa",
-            "J.K. Rowling"
-        ]
-    },
-    {
-        "In what location will you not find reindeer?": [
-            "North Pole",
-            "Lapland",
-            "Korvatunturi mountain",
-            "Finland"
-        ]
-    },
-    {
-        "What Makes Santa's Reindeer Fly?": [
-            "Magical Reindeer Dust",
-            "Fusion",
-            "Amanita muscaria",
-            "Elves"
-        ]
-    },
-    {
-        "Including Rudolph, how many reindeer hooves are there?": [
-            "36",
+        "How far can newborns see in inches?": [
+            "11",
+            "4",
             "24",
-            "16",
-            "8"
+            "36",
+            "2",
+            "56"
         ]
     },
     {
-        "Santa only has one female reindeer. Which one is it?": [
-            "Vixen",
-            "Clarice",
-            "Cupid",
-            "Cupid"
+        "Yes or no, Are babies born with survival reflexes?": [
+            "Yes",
+            "No"
         ]
     },
     {
-        "In the 1964 classic Rudolph The Red Nosed Reindeer, what was the snowman narrators name?": [
-            "Sam",
-            "Frosty",
-            "Burl",
-            "Snowy"
+        "How many hours a day does the average newborn sleep?": [
+            "15",
+            "11",
+            "19",
+            "21"
         ]
     },
     {
-        "What was Rudolph's father's name?": [
-            "Donner",
-            "Dasher",
-            "Blixen",
-            "Comet"
+        "How much of their body weight does a baby lose in the first day of their life?": [
+            "One tenth",
+            "One fourth",
+            "One fifth",
+            "One eighth"
         ]
     },
     {
-        "In the 1964 movie, What was the name of the coach of the Reindeer Games?": [
-            "Comet",
-            "Blixen",
-            "Donner",
-            "Dasher"
+        "When does a newborn usually lose his umbilical cord?": [
+            "Third week",
+            "First week",
+            "Fourth week",
+            "Fifth week"
         ]
     },
     {
-        "In the 1964 movie, what is the name of the deer that Rudolph befriends at the reindeer games?": [
-            "Fireball",
-            "Clarice",
-            "Jumper",
-            "Vixen"
+        "How many soft spots does a newborn's head have?": [
+            "Two",
+            "One",
+            "None",
+            "Three"
         ]
     },
     {
-        "In the 1964 movie, How did Donner, Rudolph's father, try to hide Rudolph's nose?": [
-            "Black mud",
-            "Bag",
-            "Pillow case",
-            "Sock"
+        "On average, a newborn's stomach holds enough food to keep him satisfied for how long?": [
+            "3 hours",
+            "1 hour",
+            "5 hours",
+            "6 hours"
         ]
     },
     {
-        "In the 1964 movie, what does the Misfit Elf want to be instead of a Santa Elf?": [
-            "Dentist",
-            "Reindeer",
-            "Toy maker",
-            "Candlestick maker"
+        "In which month are the heaviest babies born?": [
+            "May",
+            "January",
+            "September",
+            "April"
+        ]
+    },	
+    {
+        "At one year, how much weight will a baby have gained?": [
+            "Triple",
+            "Double",
+            "Quadruple"
         ]
     },
     {
-        "In the 1964 movie,what was the Bumble's one weakness?": [
-            "Could not swim",
-            "Always hungry",
-            "Candy canes",
-            "Cross eyed"
+        "How many diaper changes will a baby have by age two?": [
+            "7,300",
+			"5,400",
+            "6,300",
+            "8,200",
+			"9,800"
+        ]
+    },	
+    {
+        "Which body part does a baby develop after being born?": [
+            "Gall bladder",
+			"Taste buds",
+            "Ear drums",
+            "Knee caps",
+			"Thyroid"
         ]
     },
     {
-        "In the 1964 movie, what is Yukon Cornelius really in search of?": [
-            "Peppermint",
-            "Gold",
-            "India",
-            "Polar Bears"
+        "True or false, babies have more bones than adults?": [
+            "True",
+			"False"
         ]
     },
     {
-        "In the 1964 movie, why is the train on the Island of Misfit Toys?": [
-            "Square wheels",
-            "No Engine",
-            "Paint does not match",
-            "It does not toot"
+        "When the baby is in the womb, who eats first?": [
+            "Baby",
+			"Mom"
         ]
     },
     {
-        "In the 1964 movie, what is the name of the Jack in the Box?": [
-            "Charlie",
-            "Sam",
-            "Billy",
-            "Jack"
+        "Which taste does a baby not have when born?": [
+            "Salty",
+			"Sour",
+			"Sweet",
+			"Bitter"
         ]
     },
     {
-        "In the 1964 movie, why did Santa Claus almost cancel Christmas?": [
-            "Storm",
-            "No snow",
-            "No toys",
-            "The Reindeer were sick"
+        "Who has more taste buds, an adult or a baby?": [
+            "Baby",
+			"Adult"
         ]
     },
     {
-        "In the 1964 movie, what animal noise did the elf make to distract the Bumble?": [
-            "Oink",
-            "Growl",
-            "Bark",
-            "Meow"
+        "How heavy, in pounds, was the heaviest baby ever to be born?": [
+            "22",
+			"15",
+			"17",
+			"25"
         ]
     },
     {
-        "In the 1964 movie, what is the name of the prospector?": [
-            "Yukon Cornelius",
-            "Slider Sam",
-            "Bumble",
-            "Jack"
+        "Which is the least developed sense when a baby is born?": [
+            "Sight",
+			"Smell",
+			"Taste",
+			"Touch",
+			"Feel"
         ]
     },
     {
-        "How far do reindeer travel when they migrate?": [
-            "3000 miles",
-            "700 miles",
-            "500 miles",
-            "0 miles"
+        "How much on average do new parents spend on diapers, formula, and day care in the first year?": [
+            "7,000 dollars",
+			"6,000 dollars",
+			"5,000 dollars",
+			"4,000 dollars"
         ]
-    },
-    {
-        "How fast can a reindeer run?": [
-            "48 miles per hour",
-            "17 miles per hour",
-            "19 miles per hour",
-            "14 miles per hour"
-        ]
-    }
+    }	
+	
 ];
 
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
@@ -385,11 +284,11 @@ function onSessionEnded(sessionEndedRequest, session) {
 
 var ANSWER_COUNT = 4;
 var GAME_LENGTH = 5;
-var CARD_TITLE = "Reindeer Games"; // Be sure to change this for your skill.
+var CARD_TITLE = "Baby Trivia"; // Be sure to change this for your skill.
 
 function getWelcomeResponse(callback) {
     var sessionAttributes = {},
-        speechOutput = "Reindeer Games. I will ask you " + GAME_LENGTH.toString()
+        speechOutput = "Baby Trivia. I will ask you " + GAME_LENGTH.toString()
             + " questions, try to get as many right as you can. Just say the number of the answer. Let's begin. ",
         shouldEndSession = false,
 
