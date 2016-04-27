bf9a625f317661df03d3188a63a760118ddb4a00
dtkang <dtkang@gmail.com>
2016-04-26 23:10:15 -0500

    Update index.js for bernie geek

--- samples/spaceGeek/src/index.js
+++ samples/spaceGeek/src/index.js
@@ -21,25 +21,38 @@
 /**
  * App ID for the skill
  */
-var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = "amzn1.echo-sdk-ams.app.d7278f18-8099-4539-8fce-07b8c27425dc";
 
 /**
  * Array containing space facts.
  */
-var SPACE_FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+var BERN_FACTS = [
+    "Bernie Sanders is the first non-Christian to win a U.S. primary.",
+    "Sanders lived paycheck to paycheck growing up in Brooklyn.",
+    "His brother, Larry Sanders, is also a politician in the United Kingdom.",
+    "Sanders lost his first election in high school.",
+    "Sanders grew up reading Karl Marx.",
+    "Sanders attended the March on Washington in nineteen sixty three.",
+    "He has not tried LSD, but has been to parties where everybody else has.",
+    "Sanders delivered an eight hour filibuster against tax cuts in 2010.",
+    "Sanders has been married twice.",
+    "Sanders won his first mayoral election by 10 votes.",
+    "He has his own Ben and Jerry's ice cream flavor.",
+    "Sanders was a high school track star and a pretty good basketball player.",
+    "Sanders father, Eli Sanders, was a paint salesman.",
+    "Sanders is the longest serving independent member of Congress in American history.",
+    "If he becomes president of the United States, he will be the oldest elected president in U.S. history.",
+    "He won a second term in the Senate with 71 percent of the vote in 2012.",
+    "He was the third socialist ever elected to Congress.",
+    "He moved to Vermont in 1964.",
+    "While in college, he marched for civil rights.",
+    "He asked President Obama to raise taxes without Congress.",
+    "He lists 18 priorities on his Senate web site, including: civil liberties, dental care, and women’s rights.",
+    "He used to make documentary films.",
+    "He used to be a journalist.",
+    "He used to be a carpenter.",
+    "He was a lecturer at Harvard University.",
+    "He was born on September eighth nineteen forty one."
 ];
 
 /**
@@ -48,46 +61,46 @@ var SPACE_FACTS = [
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * SpaceGeek is a child of AlexaSkill.
+ * BernieGeek is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var SpaceGeek = function () {
+var BernieGeek = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-SpaceGeek.prototype = Object.create(AlexaSkill.prototype);
-SpaceGeek.prototype.constructor = SpaceGeek;
+BernieGeek.prototype = Object.create(AlexaSkill.prototype);
+BernieGeek.prototype.constructor = BernieGeek;
 
-SpaceGeek.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("SpaceGeek onSessionStarted requestId: " + sessionStartedRequest.requestId
+BernieGeek.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("BernieGeek onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-SpaceGeek.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("SpaceGeek onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+BernieGeek.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("BernieGeek onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     handleNewFactRequest(response);
 };
 
 /**
  * Overridden to show that a subclass can override this function to teardown session state.
  */
-SpaceGeek.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("SpaceGeek onSessionEnded requestId: " + sessionEndedRequest.requestId
+BernieGeek.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("BernieGeek onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-SpaceGeek.prototype.intentHandlers = {
+BernieGeek.prototype.intentHandlers = {
     "GetNewFactIntent": function (intent, session, response) {
         handleNewFactRequest(response);
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can ask Space Geek tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("You can ask Bernie Geek tell me a bernie fact, or, you can say exit... What can I help you with?", "What can I help you with?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
@@ -106,19 +119,19 @@ SpaceGeek.prototype.intentHandlers = {
  */
 function handleNewFactRequest(response) {
     // Get a random space fact from the space facts list
-    var factIndex = Math.floor(Math.random() * SPACE_FACTS.length);
-    var fact = SPACE_FACTS[factIndex];
+    var factIndex = Math.floor(Math.random() * BERN_FACTS.length);
+    var fact = BERN_FACTS[factIndex];
 
     // Create speech output
-    var speechOutput = "Here's your space fact: " + fact;
+    var speechOutput = "Here's your bernie fact: " + fact;
 
-    response.tellWithCard(speechOutput, "SpaceGeek", speechOutput);
+    response.tellWithCard(speechOutput, "BernieGeek", speechOutput);
 }
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the SpaceGeek skill.
-    var spaceGeek = new SpaceGeek();
-    spaceGeek.execute(event, context);
+    // Create an instance of the BernieGeek skill.
+    var bernieGeek = new BernieGeek();
+    bernieGeek.execute(event, context);
 };
 
